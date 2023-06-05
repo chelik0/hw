4f9cb37bf734ee4cdbc1adefd42dc44b2c47c688
chelik0 <mikhail.shestakov.06@gmail.com>
2023-06-06 04:17:28 +0600

    hw2

--- Tasks/FunctionTasks2/Task4.py
+++ Tasks/FunctionTasks2/Task4.py
@@ -4,3 +4,16 @@
 Первое значение кортежа - это среднее значение всех параметров, а второе значение - все числа, превышающие среднее значение.
 Второе значение оформите в виде списка.
 """
+
+def calculate(*args):
+    sum = 0
+    count = 0
+    for num in args:
+        sum += num
+        count += 1
+    a = sum / count
+    above = [num for num in args if num > a]
+    return (a, above)
+
+result = calculate(1, 2, 3, 4, 5)
+print(result)
